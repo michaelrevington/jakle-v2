9734bd0040aeb5c398b3bcf217e2e6b0be65c4b7
Ravindu Denuwan <ravindudenuwan.dev@gmail.com>
2022-12-06 17:46:15 +0530

    E1026 output

--- server/src/main/java/dev/revington/variables/StatusHandler.java
+++ server/src/main/java/dev/revington/variables/StatusHandler.java
@@ -10,6 +10,7 @@ public class StatusHandler {
     public static final JSONObject E1023 = createStatus(1023, Parameter.E1023);
     public static final JSONObject E1024 = createStatus(1024, Parameter.E1024);
     public static final JSONObject E1025 = createStatus(1025, Parameter.E1025);
+    public static final JSONObject E1026 = createStatus(1026, Parameter.E1026);
 
     public static final JSONObject createStatus(int code, String query) {
         return new JSONObject()
