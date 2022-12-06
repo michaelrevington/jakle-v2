387873d3c0e7f65dd91c1d44ba577202fb225ee1
Ravindu Denuwan <ravindudenuwan.dev@gmail.com>
2022-12-06 17:21:01 +0530

    UserRepository id usage minimalized.

--- server/src/main/java/dev/revington/repository/UserRepository.java
+++ server/src/main/java/dev/revington/repository/UserRepository.java
@@ -16,11 +16,11 @@ public interface UserRepository extends MongoRepository<User, String> {
     @Query("{'email': ?0}")
     User findByEmail(String email);
 
-    @Query(value = "{'_id' : ?0}", fields = "{email: 0, attempts: 0, socketId: 0, status: 0, validity: 0, password: 0}")
-    User findPersonal(String id);
+    @Query(value = "{'username' : ?0}", fields = "{email: 0, attempts: 0, socketId: 0, status: 0, validity: 0, password: 0}")
+    User findPersonal(String username);
     
-    @Query(value = "{'_id' : ?0}", fields = "{created: 0, active: 0, dateOfBirth: 0,email: 0, attempts: 0, socketId: 0, status: 0, validity: 0, password: 0}")
-    User findInfo(String id);
+    @Query(value = "{'username' : ?0}", fields = "{created: 0, active: 0, dateOfBirth: 0,email: 0, attempts: 0, socketId: 0, status: 0, validity: 0, password: 0}")
+    User findInfo(String username);
 
     @Aggregation(pipeline = {
             "{$unset: [\"email\",\"password\",\"socketId\",\"validity\",\"status\",\"attempts\", \"active\", \"created\"]}",
@@ -29,8 +29,6 @@ public interface UserRepository extends MongoRepository<User, String> {
             "{$lookup: {from: \"messages\", let: {\"id\": {$toString: \"$_id\"}}, pipeline: [{$match: {$expr: {$and: [{$eq: [\"$from\", $$id]}, {$eq: [\"$to\", ?0]}]}}}], as: \"messages\"}}",
             "{$match: {$expr: {$gt: [{$size: \"$data\"}, 0]}}}",
             "{$set: {\"count\": {$size: \"$messages\"}}}"
-            // "{$lookup: {from: \"friends\", localField: {$toString: \"$_id\"},
-            // foreignField: \"first\", as: \"C\"}}"
     })
     List<User> findFriends(String id);
 
@@ -39,8 +37,6 @@ public interface UserRepository extends MongoRepository<User, String> {
             "{$match: {$expr: {$and: [{$ne: [ {$toString: \"$_id\"}, ?0 ]}, {$eq: [\"$activity\", 1]}]}}}",
             "{$lookup: {from: \"friends\", let: {\"id\": {$toString: \"$_id\"}}, pipeline: [{$match: {$expr: {$or: [{$and: [{$eq: [\"$first\", $$id]}, {$eq: [\"$second\", ?0]}]}, {$and: [{$eq: [\"$second\", $$id]}, {$eq: [\"$first\", ?0]}]}]}}}], as: \"data\"}}",
             "{$match: {$expr: {$gt: [{$size: \"$data\"}, 0]}}}"
-            // "{$lookup: {from: \"friends\", localField: {$toString: \"$_id\"},
-            // foreignField: \"first\", as: \"C\"}}"
     })
     List<User> findOnlineFriends(String id);
 
@@ -49,8 +45,6 @@ public interface UserRepository extends MongoRepository<User, String> {
             "{$match: {$expr: {$and: [{$ne: [ {$toString: \"$_id\"}, ?0 ]}, {$eq: [\"$activity\", 1]}]}}}",
             "{$lookup: {from: \"friends\", let: {\"id\": {$toString: \"$_id\"}}, pipeline: [{$match: {$expr: {$or: [{$and: [{$eq: [\"$first\", $$id]}, {$eq: [\"$second\", ?0]}]}, {$and: [{$eq: [\"$second\", $$id]}, {$eq: [\"$first\", ?0]}]}]}}}], as: \"data\"}}",
             "{$match: {$expr: {$gt: [{$size: \"$data\"}, 0]}}}"
-            // "{$lookup: {from: \"friends\", localField: {$toString: \"$_id\"},
-            // foreignField: \"first\", as: \"C\"}}"
     })
     List<User> findOnlineFriendsWithSocketId(String id);
 
@@ -59,8 +53,6 @@ public interface UserRepository extends MongoRepository<User, String> {
             "{$match: {$expr: {$and: [{$ne: [ {$toString: \"$_id\"}, ?0 ]}, {$eq: [\"$activity\", 0]}]}}}",
             "{$lookup: {from: \"friends\", let: {\"id\": {$toString: \"$_id\"}}, pipeline: [{$match: {$expr: {$or: [{$and: [{$eq: [\"$first\", $$id]}, {$eq: [\"$second\", ?0]}]}, {$and: [{$eq: [\"$second\", $$id]}, {$eq: [\"$first\", ?0]}]}]}}}], as: \"data\"}}",
             "{$match: {$expr: {$gt: [{$size: \"$data\"}, 0]}}}"
-            // "{$lookup: {from: \"friends\", localField: {$toString: \"$_id\"},
-            // foreignField: \"first\", as: \"C\"}}"
     })
     List<User> findOfflineFriends(String id);
 
@@ -78,7 +70,8 @@ public interface UserRepository extends MongoRepository<User, String> {
             "{$lookup: {from: \"friends\", let: {\"id\": {$toString: \"$_id\"}}, pipeline: [{$match: {$expr: {$or: [{$and: [{$eq: [\"$first\", $$id]}, {$eq: [\"$second\", ?0]}]}, {$and: [{$eq: [\"$second\", $$id]}, {$eq: [\"$first\", ?0]}]}]}}}], as: \"friend\"}}",
             "{$lookup: {from: \"requests\", let: {\"id\": {$toString: \"$_id\"}}, pipeline: [{$match: {$expr: {$and: [{$eq: [\"$from\", ?0]}, {$eq: [\"$to\", $$id]}]}}}], as: \"requested\"}}",
             "{$lookup: {from: \"requests\", let: {\"id\": {$toString: \"$_id\"}}, pipeline: [{$match: {$expr: {$and: [{$eq: [\"$to\", ?0]}, {$eq: [\"$from\", $$id]}]}}}], as: \"pending\"}}",
-            "{$set: {\"type\": {$cond: [{$gt: [{$size: \"$friend\"}, 0]}, \"friend\", {$cond: [{$gt: [{$size: \"$requested\"}, 0]}, \"requested\", {$cond: [{$gt: [{$size: \"$pending\"}, 0]}, \"pending\", \"stranger\"]}]}]}}}"
+            "{$set: {\"type\": {$cond: [{$gt: [{$size: \"$friend\"}, 0]}, \"friend\", {$cond: [{$gt: [{$size: \"$requested\"}, 0]}, \"requested\", {$cond: [{$gt: [{$size: \"$pending\"}, 0]}, \"pending\", \"stranger\"]}]}]}}}",
+            "{$unset: [\"_id\"]}"
     })
     Slice<User> queryPeople(String id, String value, Pageable pageable);
 
