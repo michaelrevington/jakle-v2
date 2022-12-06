4644f88946c484916e58e7c5afe22a2821c2c5d2
Ravindu Denuwan <ravindudenuwan.dev@gmail.com>
2022-12-06 07:37:20 +0530

    page render progress bar

---
+++ src/components/PageLoader.jsx
@@ -0,0 +1,25 @@
+import { Component } from "react";
+
+import ComponentStyles from "../../styles/Component.module.css";
+import Styles from "../../styles/PageLoader.module.css";
+
+export default class PageLoader extends Component {
+  render() {
+    const { color, progress, transition } = this.props;
+    return (
+      <>
+        <span
+          className={ComponentStyles.absolute + " " + Styles.loadingBar}
+          style={{
+            zIndex: 50000,
+            width: `${progress}%`,
+            height: "3px",
+            backgroundColor: color,
+            boxShadow: `0 0 6px ${color}`,
+            transition: `${transition}s all ease`,
+          }}
+        ></span>
+      </>
+    );
+  }
+}
