b5ecad33a0d3783ec569024d673ab908e3879dad
Ravindu Denuwan <ravindudenuwan.dev@gmail.com>
2022-12-06 07:35:45 +0530

    base application

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import { Component as ReactComponent } from "react";
+import "../styles/globals.css";
+import "../styles/const.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+class Application extends ReactComponent {
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return <Component {...pageProps} />;
+  }
 }
 
-export default MyApp
+export default Application;
