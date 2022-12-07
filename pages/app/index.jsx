2424324a5622c8aa4cab068c297a3b83fcc00d5d
Ravindu Denuwan <ravindudenuwan.dev@gmail.com>
2022-12-07 14:25:31 +0530

    app/index

---
+++ pages/app/index.jsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import { Component } from "react";
+
+export default class Index extends Component {
+    render() {
+        return <>
+            <Head>
+                <title>Jakle - Home</title>
+            </Head>
+
+            <main>
+
+            </main>
+        </>;
+    }
+}
